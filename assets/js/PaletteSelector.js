ed0d3eb7554775c1574598c6176088f1de9f3f19
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2024-01-28 16:45:44 +0100

    Cambios en un par de paletas

--- assets/js/PaletteSelector.js
+++ assets/js/PaletteSelector.js
@@ -16,8 +16,8 @@ const colors = ['--color-A', '--color-B', '--color-C', '--color-D', '--color-E',
 const paleta1 = ['#4b1139', '#3b4058', '#2a6e78', '#7a907c', '#c9b180', '#ffd47e'];
 const paleta2 = ['#213435', '#46685b', '#648a64', '#a6b985', '#e1e3ac', '#ffd47e'];
 const paleta3 = ['#1d0c20', '#651f71', '#7037cd', '#506ee5', '#68b2f8', '#400b4a'];
-const paleta4 = ['#352640', '#92394b', '#a9767a', '#d1b4a2', '#f1f2ce', '#653587'];
-const paleta5 = ['#2a2829', '#493736', '#7b3b3b', '#668284', '#b9d7d9', '#b9d7d9'];
+const paleta4 = ['#070705', '#3e4b51', '#6f737e', '#89a09a', '#c1c0ae', '#070705'];
+const paleta5 = ['#364461', '#4d686f', '#6d756a', '#bfa374', '#e3ba6a', '#364461'];
 
 const paletas = [] // Mapa de paletas (key -> nombre de paleta, value -> lista de colores)
 paletas['paleta1'] = paleta1; paletas['paleta2'] = paleta2;
